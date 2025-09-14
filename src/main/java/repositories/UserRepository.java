3b7d8e8dbb797ab440f6f0745d1ef32926cc953e
felipeenriquezr <felipee.r.95@gmail.com>
2025-09-13 23:40:20 -0500

    connect database

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -8,3 +8,11 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<user, Long> {
     // You can add custom query methods here if needed
 }
+package com.dev.baquetasOnline..repository;
+
+import com.example.baquetaspro.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
+
