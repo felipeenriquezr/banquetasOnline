872ddd22e8519ce85b0c95da2bc9dbc50e6886c3
felipeenriquezr <felipee.r.95@gmail.com>
2025-09-27 16:36:11 -0500

    Changes for db connection and dependences

--- src/main/java/com/BaquetasOnline/Model/repository/productsRepository.java
+++ src/main/java/com/BaquetasOnline/Model/repository/productsRepository.java
@@ -2,6 +2,10 @@ package com.BaquetasOnline.Model.repository;
 
 import org.hibernate.sql.results.jdbc.internal.JdbcValuesMappingProducerStandard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 
 public interface productsRepository extends JpaRepository<JdbcValuesMappingProducerStandard, Long> {
 }
+
